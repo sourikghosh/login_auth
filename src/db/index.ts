58385c413507417ccb3196cbcf6f080b2e9616a4
Sourik Ghosh <sourikghosh31@gmail.com>
2020-09-15 16:15:41 +0530

    added

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,6 +8,28 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString
 })
-
+// //for client checkout
+// ; (async () => {
+//     const client = await pool.connect()
+//     try {
+//         const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
+//         console.log(res.rows[0])
+//     }
+//     finally {
+//         // Make sure to release the client before any error handling,
+//         // just in case the error handling itself throws an error.
+//         client.release()
+//     }
+// })()
+//     .catch(err => console.log(err.stack))
+// //single query recommended
+// ; (async () => {
+//     const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [1])
+//     console.log('user:', rows[0])
+// })().catch(err =>
+//     setImmediate(() => {
+//         throw err
+//     })
+// )
 export default pool
 

f76bc1db4bad3b9cbb7bc92fe577ccf4be22041e
Sourik Ghosh <sourikghosh31@gmail.com>
2020-09-26 13:44:20 +0530

    added hash password

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
 import { body, validationResult } from 'express-validator'
 import { getByEmail } from '../db/getByEmail'
+import bcrypt from 'bcrypt'
 
 export const signupValidationResult = (req: Request, res: Response, next: NextFunction) => {
     const result = validationResult(req);
     const hasErrors = !result.isEmpty();
-    hasErrors ? res.send(result.mapped()) : res.json({ "message": "👌🤴🏽" })
+    hasErrors ? res.send(result.mapped()) : bcrypt.hash(req.body.password, 12, function (err, hash) {
+        // Store hash in your password DB.
+    });
+    //res.json({ "message": "👌🤴🏽" })
 }
 
 export const signupValidation = [

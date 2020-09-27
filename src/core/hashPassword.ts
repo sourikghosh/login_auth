6526370e759c29b88d418b3eda95370f366aa133
Sourik Ghosh <sourikghosh31@gmail.com>
2020-09-27 22:46:48 +0530

    bug fixed

--- src/core/hashPassword.ts
+++ src/core/hashPassword.ts
@@ -1,7 +1,7 @@
-import bcrypt from 'bcrypt'
+import { hash } from 'bcrypt'
 
 export const hashPassword = async (password: String) => {
-    try { return await bcrypt.hash(password, 12) }
+    try { return await hash(password, 12) }
     catch (err) {
         throw err
     }

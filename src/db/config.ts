df0383dbf68d4fdbabd58b30c3b9587d0ac6221d
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-21 15:04:02 +0530

    db config changed

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg'
-let connectionString
-(process.env.NODE_ENV === 'production') ? (connectionString = process.env.DATABASE_URL)
-    : (connectionString = 'postgresql://sourik:neverdie@localhost:5432/login_auth')
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://sourik:neverdie@localhost:5432/login_auth'
 
 export const pool = new Pool({
     connectionString

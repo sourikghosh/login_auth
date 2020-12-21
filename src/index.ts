03b867398cbda5dde142caea704205889a1288ce
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-21 15:04:33 +0530

    view engine and public file servered

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express'
-import routes from './api/routes'
+import path from 'path'
+import routes from '../src/api/routes'
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
 app.use(routes)
 
 const PORT = process.env.PORT || "4000"

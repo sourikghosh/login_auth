b64e630fc9c9d80b0821a14592999c27243c4730
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-24 13:45:30 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import path from 'path'
+import createError, { HttpError } from 'http-errors'
 import routes from '../src/api/routes'
 
 const app = express()
@@ -8,10 +9,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(routes)
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+    next(new createError.NotFound())
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        },
+    })
+})
 
 const PORT = process.env.PORT || "4000"
 

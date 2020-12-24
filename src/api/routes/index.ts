cf8927dbb598234b53cb1de3d4d4ea6724165e74
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-24 14:37:14 +0530

    new routes added

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from 'express'
 import { ValidationResult, signupValidation, loginValidation } from '../middleware/auth'
 import { signUp } from '../../core/siginup'
 import { login } from '../../core/login'
+import { refreshToken } from '../../core/refreshToken'
+import { logout } from '../../core/logout'
+
+
 const routes = Router()
 
 routes.get('/', async (req, res) => { res.redirect('/login') })
@@ -11,5 +15,7 @@ routes.get('/dashboard', async (req, res) => { res.render('dashboard') })
 
 routes.post('/signup', signupValidation, ValidationResult, signUp)
 routes.post('/login', loginValidation, ValidationResult, login)
+routes.post('/refreshToken', refreshToken)
+routes.delete('/logout', logout)
 
 export default routes

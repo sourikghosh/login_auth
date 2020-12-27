e9bdb56be5770143b55702e2fd962822d6d1fd31
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-27 20:57:56 +0530

    modified

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,9 +1,11 @@
+import { Router } from 'express'
 import { ValidationResult, signupValidation, loginValidation } from '../middleware/auth'
 import { signUp } from '../../core/siginup'
 import { login } from '../../core/login'
 import { refreshToken } from '../../core/refreshToken'
 import { logout } from '../../core/logout'
-import routes from './clientRoute'
+
+const routes = Router()
 
 routes.post('/api/signup', signupValidation, ValidationResult, signUp)
 routes.post('/api/login', loginValidation, ValidationResult, login)

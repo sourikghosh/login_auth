a4178ab861b7c8ca7039512617fd321163c5b45b
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-27 16:21:18 +0530

    updated

---
+++ src/api/routes/clientRoute.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+const routes = Router()
+routes.get('/', async (req, res) => { res.redirect('/login') })
+routes.get('/signup', async (req, res) => { res.render('signup') })
+routes.get('/login', async (req, res) => { res.render('login') })
+routes.get('/dashboard', async (req, res) => { res.render('dashboard', { User: "default" }) })
+routes.get('/err', async (req, res) => { res.render('error', { status: '404', msg: 'not found' }) })
+
+export default routes

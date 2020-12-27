1b54b05d27f0ce11199fb50a0b5bc46acab160c9
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-27 15:28:37 +0530

    modified dashboard

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -11,7 +11,7 @@ const routes = Router()
 routes.get('/', async (req, res) => { res.redirect('/login') })
 routes.get('/signup', async (req, res) => { res.render('signup') })
 routes.get('/login', async (req, res) => { res.render('login') })
-routes.get('/dashboard', async (req, res) => { res.render('dashboard') })
+routes.get('/dashboard', async (req, res) => { res.render('dashboard', { User: "default" }) })
 routes.get('/err', async (req, res) => { res.render('error', { status: '404', msg: 'not found' }) })
 
 routes.post('/api/signup', signupValidation, ValidationResult, signUp)

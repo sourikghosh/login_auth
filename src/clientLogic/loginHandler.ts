d65eb66bddd3f2885e2b0de61bc866b61308683d
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-27 16:44:40 +0530

    client logic

---
+++ src/clientLogic/loginHandler.ts
@@ -0,0 +1,12 @@
+//axios post req to "api/login"
+//and set the header with the jwt from response
+import axios from 'axios'
+//idk how to perform that
+export const loginHandler = async () => {
+    //axios.post('api/login')
+    //if success : render login.ejs
+    //else render error.ejs
+}
+
+//my doubt is that how to add this handler to by login/signup BUTTON ??
+//and how does it works if 

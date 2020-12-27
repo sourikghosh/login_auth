722a1ba368df4b09dc07bf914a1fd5870194455a
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-27 20:50:37 +0530

    removed client logic

--- src/clientLogic/signuphandler.ts
+++
@@ -1,9 +0,0 @@
-//axios post req to "api/signup"
-//and set the header with the jwt from response
-import axios from 'axios'
-//idk how to perform that
-export const loginHandler = async () => {
-    //axios.post('api/signup')
-    //if success : render protected route '/dashboard'
-    //else render error.ejs
-}
